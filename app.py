91e0ca472d52f89f473d3460c238d005817a7eef
wangye1221 <f_totti_ac@sina.com>
2020-02-28 17:31:27 +0800

    Add index page.

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+name = '我，大烨'
+movielist = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-def hello() -> str:
-    return '<h1>你好，龙猫！</h1><img src="http://helloflask.com/totoro.gif">'
+def index() -> 'html':
+    return render_template('index.html', name=name, movies=movielist)
 
 @app.route('/user/<name>')
 def user_page(name: str) -> str:
@@ -12,7 +26,7 @@ def user_page(name: str) -> str:
 
 @app.route('/test')
 def test_url_for() -> str:
-    print(url_for('hello'))
+    print(url_for('index'))
     print(url_for('user_page', name = 'BigYe'))
     print(url_for('user_page', name = 'WillWang'))
     print(url_for('test_url_for'))
